6f5c76c52556c2ab5ab2500537be60a622114b8d
Patricio Perez <patricio.perez@halopowered.com>
2026-01-20 19:34:44 -0300

    Add initial Xcode project

---
+++ Challenge-Cities/Challenge-Cities/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge-Cities
+//
+//  Created by Patricio Perez on 20/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
