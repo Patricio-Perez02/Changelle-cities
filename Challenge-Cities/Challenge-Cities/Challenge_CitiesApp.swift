06e4b8b7e8beb1488523104c262d43d220ea7d67
Patricio Perez <patriciojulianperez@gmail.com>
2026-01-20 19:34:44 -0300

    Add initial Xcode project

---
+++ Challenge-Cities/Challenge-Cities/Challenge_CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_CitiesApp.swift
+//  Challenge-Cities
+//
+//  Created by Patricio Perez on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_CitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge-Cities/Challenge-CitiesTests/Challenge_CitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_CitiesTests.swift
+//  Challenge-CitiesTests
+//
+//  Created by Patricio Perez on 20/01/2026.
+//
+
+import Testing
+@testable import Challenge_Cities
+
+struct Challenge_CitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
